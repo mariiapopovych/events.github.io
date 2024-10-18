1db8963c06695809be43a4219a6bff6db6f9d4d4
Yevhenii <yevhenii.serdiuk@powercoders.org>
2024-10-18 12:06:19 +0200

    showfooter

--- index.js
+++ index.js
@@ -136,3 +136,21 @@ function displayEvents(events, selectedDay = '', selectedTime = '') {
 }
 
 displayEvents(events);
+
+
+}
+
+displayEvents(events);
+=======
+});
+
+
+function showfooter() {
+  let footer = document.getElementsByTagName("footer")[0];
+  footer.classList.add("footerVisible");
+};
+
+function hidefooter(){
+  let footer = document.getElementsByTagName("footer")[0];
+  footer.classList.remove("footerVisible");
+};
