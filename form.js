da634f2116c6d24391f05fcb749e9aa6a3dad069
burak celik <brkclk.ch@gmail.com>
2024-10-18 10:49:52 +0200

    fix css

--- form.js
+++ form.js
@@ -9,20 +9,24 @@ console.log(text)
 let button = document.getElementById("submit");
 console.log(button)
 
+
+
 button.addEventListener("click", function(e) {
     e.preventDefault()
 
-    if (userName.value.length === 0){
+    if (userName.value.length === 0) { 
         userName.style.border = "2px solid red";
-        alert("please fill correctly")
+        alert("please fill name correctly")
 
     } else if (!email.value.includes("@")) {
         email.style.border = "2px solid red";
-        alert("please fill correctly")
+        alert("please fill your email correctly")
+
     } else if (text.value.length === 0) {
         text.style.border = "2px solid red";
-        alert("please fill correctly")
+        alert("please fill you Number correctly")
     }
 
-
 })
+
+
